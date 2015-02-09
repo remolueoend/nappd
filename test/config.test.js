ddf718b14b093857ddf3896d6792343518c271eb
Remo Lüönd <remo.zumsteg@outlook.com>
2015-02-09 23:52:37 +0100

    implemented basic functionality and test environment

---
+++ test/config.test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by remo on 09/02/15.
+ */
+
+exports.addApp = function(test){
+    test.ok(false, 'Expected to be true too!');
+    test.done();
+};
---
+++ test/nappd.test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by remo on 09/02/15.
+ */
+
+exports.register = function(test){
+    test.ok(true, 'expected to be true!');
+    test.done();
+};
